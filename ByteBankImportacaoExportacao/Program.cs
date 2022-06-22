75c78a3fd3494578d20b5c6b6e950abc4d22518d
Antonio Dias <antonio.dias@bit.com.br>
2022-06-22 13:41:50 -0300

    Lodando com IO'S Com a classe StreamReader

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -10,40 +10,18 @@ using System.IO; // IO = Input e Output
 
 namespace ByteBankImportacaoExportacao
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-
             var enderecoDoArquivo = "contas.txt";
-
-            using (var fluxoDoArquivo = new FileStream("contas.txt", FileMode.Open))
+            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
             {
-                var buffer = new byte[1024]; // 1 kb
-                var numeroDeBytesLidos = -1;
-
-                while (numeroDeBytesLidos != 0)
-                {
-                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-                    EscreverBuffer(buffer, numeroDeBytesLidos);
-                }
+                var leitor = new StreamReader(fluxoDeArquivo);
+                var linha = leitor.ReadToEnd();
             }
-            Console.ReadLine();
+
         }
-
-        static void EscreverBuffer(byte[] buffer, int byteslidos)
-        {
-            var utf8 = Encoding.Default;
-
-            var texto = utf8.GetString(buffer, 0, byteslidos);
-            Console.Write(texto);
-
-            //foreach (var meuByte in buffer)
-            //{
-            //    Console.Write(meuByte);
-            //    Console.Write(" ");
-            //}
-        }
-
     }
 }
+
